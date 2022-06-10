5b1cf7a39612f4412d1586b8f3acc66f1ee45775
pe-gogo <623586617@qq.com>
2022-06-10 16:23:32 +0800

    -放入Vue中

---
+++ vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav">
+    <Header></Header>
+    <div style="display: flex">
+      <Aside></Aside>
+      <router-view style="flex: 1"></router-view>
+
+    </div>
+  </div>
+</template>
+
+
+<style>
+
+</style>
+
+<script>
+import Header from "./components/Header";
+import Aside from "./components/Aside";
+export default {
+  name: "Layout",
+  components:{
+    Aside,
+    Header
+  }
+}
+</script>
